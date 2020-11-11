c49e17f67e6f6bc40c4853dc7d1071d8af114103
Victor Branger <victor.branger@gmail.com>
2020-11-11 13:58:28 +0100

    embedded new review form in restaurant page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,18 @@
 class ReviewsController < ApplicationController
 
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
+  # def new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = Review.new
+  # end
 
   def create
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    # raise
-    # @review.rating = @review.rating.to_i
     if @review.save!
       redirect_to restaurant_path(@restaurant)
     else
-      render :newRe
+      render :new
     end
   end
 
